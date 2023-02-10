da4b832baf21f974d1dbdc89bf8bb9740fb857b1
Erkhembayar Gantulga <erkhem@tomyo.mn>
2023-02-10 12:05:37 +0800

    style: fixed identation

--- src/ckeditor.js
+++ src/ckeditor.js
@@ -37,93 +37,93 @@ class Editor extends ClassicEditor {}
 
 // Plugins to include in the build.
 Editor.builtinPlugins = [
-	Alignment,
-	Autoformat,
-	BlockQuote,
-	Bold,
-	Code,
-	CodeBlock,
-	Essentials,
-	FontBackgroundColor,
-	FontColor,
-	Heading,
-	Image,
-	ImageCaption,
-	ImageStyle,
-	ImageToolbar,
-	ImageUpload,
-	Indent,
-	Italic,
-	Link,
-	List,
-	MediaEmbed,
-	Paragraph,
-	PasteFromOffice,
-	SourceEditing,
-	SpecialCharacters,
-	SpecialCharactersArrows,
-	Table,
-	TableToolbar,
-	TextTransformation,
-	Underline
+  Alignment,
+  Autoformat,
+  BlockQuote,
+  Bold,
+  Code,
+  CodeBlock,
+  Essentials,
+  FontBackgroundColor,
+  FontColor,
+  Heading,
+  Image,
+  ImageCaption,
+  ImageStyle,
+  ImageToolbar,
+  ImageUpload,
+  Indent,
+  Italic,
+  Link,
+  List,
+  MediaEmbed,
+  Paragraph,
+  PasteFromOffice,
+  SourceEditing,
+  SpecialCharacters,
+  SpecialCharactersArrows,
+  Table,
+  TableToolbar,
+  TextTransformation,
+  Underline
 ];
 
 // Editor configuration.
 Editor.defaultConfig = {
-	toolbar: {
-		items: [
-			'undo',
-			'redo',
-			'|',
-			'heading',
-			'bold',
-			'italic',
-			'underline',
-			'|',
-			'fontColor',
-			'fontBackgroundColor',
-			'numberedList',
-			'bulletedList',
-			'alignment',
-			'|',
-			'link',
-			'blockQuote',
-			'code',
-			'imageUpload',
-			'specialCharacters',
-			'insertTable',
-			'mediaEmbed',
-			'sourceEditing'
-		]
-	},
-        heading: {
-          options: [
-            { model: 'paragraph', title: 'Paragraph', class: 'ck-heading_paragraph' },
-            { model: 'heading1', view: 'h1', title: 'M1 (middle black body)', class: 'ck-heading_heading1' },
-            { model: 'heading2', view: 'h2', title: 'S1 (middle bold black body)', class: 'ck-heading_heading2' },
-            { model: 'heading3', view: 'h3', title: 'S2 (small black body)', class: 'ck-heading_heading3' },
-            { model: 'heading4', view: 'h4', title: 'E1 (small bold black)', class: 'ck-heading_heading4' },
-            { model: 'heading5', view: 'h5', title: 'E2 (middle grey)', class: 'ck-heading_heading5' },
-            { model: 'heading6', view: 'h6', title: 'E3 (small grey)', class: 'ck-heading_heading6' }
-          ]
-        },
-	language: 'en',
-	image: {
-		toolbar: [
-			'imageTextAlternative',
-			'toggleImageCaption',
-			'imageStyle:inline',
-			'imageStyle:block',
-			'imageStyle:side'
-		]
-	},
-	table: {
-		contentToolbar: [
-			'tableColumn',
-			'tableRow',
-			'mergeTableCells'
-		]
-	}
+  toolbar: {
+    items: [
+      'undo',
+      'redo',
+      '|',
+      'heading',
+      'bold',
+      'italic',
+      'underline',
+      '|',
+      'fontColor',
+      'fontBackgroundColor',
+      'numberedList',
+      'bulletedList',
+      'alignment',
+      '|',
+      'link',
+      'blockQuote',
+      'code',
+      'imageUpload',
+      'specialCharacters',
+      'insertTable',
+      'mediaEmbed',
+      'sourceEditing'
+    ]
+  },
+  heading: {
+    options: [
+      { model: 'paragraph', title: 'Paragraph', class: 'ck-heading_paragraph' },
+      { model: 'heading1', view: 'h1', title: 'M1 (middle black body)', class: 'ck-heading_heading1' },
+      { model: 'heading2', view: 'h2', title: 'S1 (middle bold black body)', class: 'ck-heading_heading2' },
+      { model: 'heading3', view: 'h3', title: 'S2 (small black body)', class: 'ck-heading_heading3' },
+      { model: 'heading4', view: 'h4', title: 'E1 (small bold black)', class: 'ck-heading_heading4' },
+      { model: 'heading5', view: 'h5', title: 'E2 (middle grey)', class: 'ck-heading_heading5' },
+      { model: 'heading6', view: 'h6', title: 'E3 (small grey)', class: 'ck-heading_heading6' }
+    ]
+  },
+  language: 'en',
+  image: {
+    toolbar: [
+      'imageTextAlternative',
+      'toggleImageCaption',
+      'imageStyle:inline',
+      'imageStyle:block',
+      'imageStyle:side'
+    ]
+  },
+  table: {
+    contentToolbar: [
+      'tableColumn',
+      'tableRow',
+      'mergeTableCells'
+    ]
+  }
 };
 
 export default Editor;
